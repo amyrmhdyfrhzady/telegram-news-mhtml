6546f93e44ecc2fa73c57c882ecb537e6e9f96d2
Amirmahdifarahzadi <amricangaildivert@gmail.com>
2026-07-08 22:23:37 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,70 +1,22 @@
 import config from "./config.js";
-
 import { fetchChannel } from "./browser.js";
 import { sendFile } from "./bale.js";
+import { info, error } from "./logger.js";
 
-import { sha256 } from "./hash.js";
+(async () => {
+    info("Job started");
 
-import {
-  loadState,
-  saveState,
-  getLastMessageId,
-  setChannelState,
-} from "./state.js";
+    for (const channel of config.channels) {
+        try {
+            const { file } = await fetchChannel(channel);
 
-import { info, warn, error } from "./logger.js";
+            await sendFile(channel, file);
 
-async function main() {
-  const state = await loadState();
-
-  for (const channel of config.channels) {
-    try {
-      info(`Checking ${channel.name}`);
-
-      const { file, posts } = await fetchChannel(channel);
-
-      if (!posts.length) {
-        warn(`${channel.name}: no posts found`);
-
-        continue;
-      }
-
-      const newestPost =
-    posts[posts.length - 1];
-
-const hash = sha256(
-
-    JSON.stringify(
-
-        posts.map(post => ({
-            id: post.id,
-            text: post.text,
-            caption: post.caption
-        }))
-
-    )
-
-);
-
-      await sendFile(file);
-
-      setChannelState(
-        state,
-
-        channel.name,
-
-        newestPost.id,
-
-        hash,
-      );
-
-      await saveState(state);
-
-      info(`${channel.name}: ${newPosts.length} new post(s) processed`);
-    } catch (e) {
-      error(`${channel.name}: ${e.message}`);
+            info(`${channel.name} sent successfully`);
+        } catch (err) {
+            error(`${channel.name}: ${err.message}`);
+        }
     }
-  }
-}
 
-main().catch(error);
+    info("Job finished");
+})();
